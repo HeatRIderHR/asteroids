ad23692c5f2a48ecb5ce68919d766af8cdc5ae8e
Kyle.T.Y <kylethompsonyoung@gmail.com>
2025-08-09 13:14:08 +0100

    3: Time Clock added 60FPS limt added

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    timeClock = pygame.time.Clock()
+    dt = 0 
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill("#000000")
+            
         pygame.display.flip()
-
+        dt = timeClock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
