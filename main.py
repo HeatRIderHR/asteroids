cacd00cca6572d5550f901687ce9d280a9913d32
Kyle.T.Y <kylethompsonyoung@gmail.com>
2025-08-09 12:48:15 +0100

    Intial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen Width {SCREEN_WIDTH}")
+    print(f"Screen Width {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
